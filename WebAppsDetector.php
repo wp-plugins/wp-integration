460413982520220cc3861f25e72a30e5b17680e6
inveo <inveo@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 16:01:47 +0000

    1.4.05
    git-svn-id: https://plugins.svn.wordpress.org/wp-integration/tags/1.4.05@1154639 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- WebAppsDetector.php
+++ WebAppsDetector.php
@@ -7,17 +7,20 @@
 
 /** @class: WebAppsDetector
   * @project: Theme Provider
-  * @date: 2015-03-14
+  * @date: 2015-03-28
   * @compatibility: PHP 5 >= 5.0.0
-  * @version: 1.0.6
+  * @version: 1.1.2
   */
 class WebAppsDetector
 {
 	const PROVIDER_FILE = 'ThemeProvider.php';
+	const PROVIDER_FILE_LITE = 'ThemeProviderFree.php';
 
 	private static $_appFound = false;
 	private static $_appName = 'host web application';
 	private static $_appNameAbbr = 'web app';
+	private static $_appLatest = '';
+	private static $_providerAdvance = true;
 	private static $_providerPath = '';
 	private static $_providerFilePath = '';
 	private static $_providerFound = false;
@@ -32,6 +35,7 @@ class WebAppsDetector
 						array(
 							'name' => 'PrestaShop',
 							'abbr' => 'PS',
+							'latest' => '1.4.03',
 							'path' => 'modules/psthemeprovider',
 							'files' => array('config/settings.inc.php', 'config/defines.inc.php')
 						)
@@ -46,14 +50,23 @@ class WebAppsDetector
 			self::$_appFound = true;
 			self::$_appName = $provider['name'];
 			self::$_appNameAbbr = $provider['abbr'];
+			self::$_appLatest = $provider['latest'];
 			self::$_providerPath = $provider['path'];
 			$path = str_replace('/' , DIRECTORY_SEPARATOR, $provider['path']);
 			if(file_exists(self::_makeFsPath(self::PROVIDER_FILE, $path)))
 			{
 				self::$_providerFound = true;
+				self::$_providerAdvance = true;
 				self::$_providerFilePath = self::_makeFsPath(self::PROVIDER_FILE, $path);
-				return true;
 			}
+			elseif(file_exists(self::_makeFsPath(self::PROVIDER_FILE_LITE, $path)))
+			{
+				self::$_providerFound = true;
+				self::$_providerAdvance = false;
+				self::$_providerFilePath = self::_makeFsPath(self::PROVIDER_FILE_LITE, $path);
+			}
+			if(self::$_providerFound)
+				return true;
 			return false;
 		}
 		return false;
@@ -67,6 +80,11 @@ class WebAppsDetector
 	public static function providerFound()
 	{
 		return self::$_providerFound;
+	}
+	
+	public static function providerIsAdvance()
+	{
+		return self::$_providerAdvance;
 	}
 
 	public static function getPath()
@@ -87,6 +105,11 @@ class WebAppsDetector
 	public static function getNameAbbr()
 	{
 		return self::$_appNameAbbr;
+	}
+	
+	public static function getLatest()
+	{
+		return self::$_appLatest;
 	}
 	
 	private static function _makeFsPath($file, $path = '')
